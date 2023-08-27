af1addd9257743a13f3e841a40b7fb7d7222cf13
Jingting Liu <89711106+JingtingL@users.noreply.github.com>
2023-08-26 23:17:25 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 //Blinky
 #include "mbed.h"
 
+DigitalIn input(BUTTON1);
 DigitalOut led(LED1);
 
 int main() {
     while (1) {
-        led = !led;
+        int x = input.read();
+        led = x;
         printf("Blink! LED is now %d\n", led.read());
 
-        wait_ms(500);
+        wait_ms(250);
     }
 }
+
