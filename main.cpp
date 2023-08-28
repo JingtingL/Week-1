c473c50f0c234c1e1b3519f52785e2e5c5530e2a
Jingting Liu <89711106+JingtingL@users.noreply.github.com>
2023-08-28 15:05:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
 //Blinky
+
+//Exercise 1
 #include "mbed.h"
 
+//Specifying we can click to Button1 to trigger a response
 DigitalIn input(BUTTON1);
+//Specifying the LED1 will be responding to our code
 DigitalOut led(LED1);
 
 int main() {
     while (1) {
+        //reading whether there is an input
         int x = input.read();
+        //led will take the input
         led = x;
         printf("Blink! LED is now %d\n", led.read());
 
+        //response time is 0.25 second
         wait_ms(250);
     }
 }
 
+
+
+
 //Exercise 2
 #include "mbed.h"
 
+//Specifying we can input into the pentiometer to trigger a response
 AnalogIn anInput(p15);
+//Specifying the LED1 will be outputting
 DigitalOut led(LED1);
 
 int main() {
+    //in C/C++ anything other than 0 means true
     while (1) {
+        //led will take the input
         led = anInput;
         printf("Blink! LED is now %d\n", led.read());
 
+        //response time is 2 second
         wait_ms(2000);
     }
 }
